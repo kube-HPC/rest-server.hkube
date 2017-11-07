1292a0266c467fa19f782a1e908a09fc89f04158
Nassi Harel <nassi9@gmail.com>
2017-11-07 15:15:43 +0200

    updates

---
+++ middlewares/powered-by.js
@@ -0,0 +1,11 @@
+/*
+ * Created by nassi on 7/11/17.
+ * X-Powered-By middleware
+ */
+
+const powered = (poweredBy) => (req, res, next) => {
+    res.setHeader('X-Powered-By', poweredBy);
+    next();
+};
+
+module.exports = powered;
