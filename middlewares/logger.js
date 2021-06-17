0013cc917a10c7af95a2f096f6ed851604218117
NassiHarel <nassi9@gmail.com>
2021-06-17 11:22:23 +0300

    feat: add logger

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,16 +1,29 @@
 
 const now = require('performance-now');
 
-const logger = emitter => (req, res, next) => {
-    const start = now();
-    const { method, url } = req;
-    res.on('finish', () => {
-        const end = now();
-        const status = res.statusCode;
-        const duration = (end - start).toFixed(2);
-        emitter.emit('request', { method, url, status, duration });
-    });
-    next();
+const logger = (options) => {
+    const { filterRoutes = [], onRequest, onResponse } = options;
+
+    return (req, res, next) => {
+        const { method, url } = req;
+
+        if (filterRoutes.some(f => url.startsWith(f))) {
+            return next();
+        }
+        if (onRequest) {
+            onRequest({ method, url });
+        }
+        if (onResponse) {
+            const start = now();
+            res.on('finish', () => {
+                const end = now();
+                const status = res.statusCode;
+                const duration = (end - start).toFixed(2);
+                onResponse({ method, url, status, duration });
+            });
+        }
+        return next();
+    };
 };
 
 module.exports = logger;
